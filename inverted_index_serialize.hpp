386298d08cf469cc27d69ee73b91af6b73e29aab
ct.clmsn <ct.clmsn@gmail.com>
2021-07-16 22:07:40 -0400

    added ii serializer

---
+++ inverted_index_serialize.hpp
@@ -0,0 +1,32 @@
+//  Copyright (c) 2021 Christopher Taylor
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(INVERTED_INDEX_SERIALIZATION_HPP)
+#define INVERTED_INDEX_SERIALIZATION_HPP
+
+#include <hpx/serialization/unordered_map.hpp>
+#include <hpx/include/util.hpp>
+#include <cstddef>
+
+#include "documents.hpp"
+
+namespace hpx { namespace serialization
+{
+    template <typename T, bool TF>
+    void load(
+        input_archive& archive, inverted_index_t& target, unsigned)
+    {
+        archive >> target;
+    }
+
+    template <typename T, bool TF>
+    void save(output_archive& archive,
+        inverted_index_t const& target, unsigned)
+    {
+        archive << target;
+    }
+}}
+
+#endif
